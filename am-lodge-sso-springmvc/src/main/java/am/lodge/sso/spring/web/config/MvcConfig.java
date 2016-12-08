749bc30396800460258e016e02b3f9921aebb56d
am <am-wj@163.com>
2016-12-08 09:20:43 +0800

    修改合并错误

--- am-lodge-sso-springmvc/src/main/java/am/lodge/sso/spring/web/config/MvcConfig.java
+++ am-lodge-sso-springmvc/src/main/java/am/lodge/sso/spring/web/config/MvcConfig.java
@@ -1,59 +1,10 @@
 package am.lodge.sso.spring.web.config;
 
-<<<<<<< HEAD
-=======
 import am.lodge.sso.spring.web.exception.GlobalHandlerExceptionResolver;
->>>>>>> origin/master
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-<<<<<<< HEAD
-import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
-import org.springframework.web.servlet.HandlerAdapter;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.handler.SimpleServletHandlerAdapter;
-import org.springframework.web.servlet.mvc.Controller;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-@Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "am.lodge.sso.spring.web.controller", useDefaultFilters = false, includeFilters = {
-    @ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Controller.class})
-})
-public class MvcConfig extends WebMvcConfigurationSupport{
-
-  @Bean
-  public ViewResolver JsonResolver() {
-    ContentNegotiatingViewResolver c;
-    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-    viewResolver.setPrefix("/WEB-INF/views/");
-    viewResolver.setSuffix(".jsp");
-    return viewResolver;
-  }
-
-  @Bean
-  public ViewResolver JspResolver() {
-    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-    viewResolver.setPrefix("/WEB-INF/views/");
-    viewResolver.setSuffix(".jsp");
-    viewResolver.setOrder(2);
-    return viewResolver;
-  }
-
-  @Bean
-  public HandlerAdapter servletHandlerAdapter(){
-    return new SimpleServletHandlerAdapter();
-  }
-
-  @Override
-  protected ConfigurableWebBindingInitializer getConfigurableWebBindingInitializer() {
-    ConfigurableWebBindingInitializer initializer = super.getConfigurableWebBindingInitializer();
-    return initializer;
-=======
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
@@ -99,6 +50,5 @@ public class MvcConfig extends WebMvcConfigurationSupport{
   @Override
   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
     configurer.enable();
->>>>>>> origin/master
   }
 }
