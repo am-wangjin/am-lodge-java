9825605056e6312dd8037597d921b6454e0e424b
am-wangjin <am-wangjin@gmail.com>
2017-02-03 13:05:30 +0800

    增加默认处理视图

--- am-lodge-spring-webmvc/src/test/java/am/lodge/spring/mvc/test/config/MvcConfig.java
+++ am-lodge-spring-webmvc/src/test/java/am/lodge/spring/mvc/test/config/MvcConfig.java
@@ -13,10 +13,16 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.ServletContext;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by am on 16-11-13.
@@ -28,15 +34,16 @@ public class MvcConfig implements ApplicationContextAware, ServletContextAware{
   private ContentNegotiationManager contentNegotiationManager;
 
   @Bean
-  public ContentNegotiationManager mvcContentNegotiationManager() {
-    if(this.contentNegotiationManager == null) {
+  public ContentNegotiationManager mvcContentNegotiationManager(){
+    if(this.contentNegotiationManager == null){
       ContentNegotiationManagerFactoryBean factory = new ContentNegotiationManagerFactoryBean();
       factory.setServletContext(this.servletContext);
       factory.setIgnoreAcceptHeader(true);
       factory.addMediaType("json", MediaType.APPLICATION_JSON);
-      try {
+      try{
+        factory.afterPropertiesSet();
         this.contentNegotiationManager = factory.getObject();
-      } catch (Exception ex) {
+      }catch(Exception ex){
         throw new BeanInitializationException("Could not create ContentNegotiationManager", ex);
       }
     }
@@ -44,20 +51,34 @@ public class MvcConfig implements ApplicationContextAware, ServletContextAware{
   }
 
   @Bean
-  public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+  public RequestMappingHandlerMapping requestMappingHandlerMapping(){
     RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
     handlerMapping.setContentNegotiationManager(this.mvcContentNegotiationManager());
     return handlerMapping;
   }
 
   @Bean
-  public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+  public RequestMappingHandlerAdapter requestMappingHandlerAdapter(){
     RequestMappingHandlerAdapter adapter = new RequestMappingHandlerAdapter();
     adapter.setContentNegotiationManager(this.mvcContentNegotiationManager());
     ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().applicationContext(this.applicationContext).build();
     MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(objectMapper);
     adapter.getMessageConverters().add(converter);
     return adapter;
+  }
+
+  @Bean
+  public ViewResolver viewResolver(){
+    ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
+    viewResolver.setContentNegotiationManager(this.mvcContentNegotiationManager());
+    viewResolver.setOrder(1);
+    MappingJackson2JsonView view = new MappingJackson2JsonView();
+    view.setServletContext(this.servletContext);
+    view.setApplicationContext(this.applicationContext);
+    List<View> views = new ArrayList();
+    views.add(view);
+    viewResolver.setDefaultViews(views);
+    return viewResolver;
   }
 
   @Override
