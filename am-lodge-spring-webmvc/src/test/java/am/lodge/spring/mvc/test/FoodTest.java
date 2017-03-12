ad7e457686914cdfbf2753d80cdc971637059006
am-wangjin <am-wangjin@gmail.com>
2017-03-12 11:16:54 +0800

    增加msg返回类型

--- am-lodge-spring-webmvc/src/test/java/am/lodge/spring/mvc/test/FoodTest.java
+++ am-lodge-spring-webmvc/src/test/java/am/lodge/spring/mvc/test/FoodTest.java
@@ -17,7 +17,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void findAllForAcceptHeader() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":[{\"id\":\"1\",\"name\":\"面包\"},{\"id\":\"2\",\"name\":\"米饭\"}]}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":[{\"id\":\"1\",\"name\":\"面包\"},{\"id\":\"2\",\"name\":\"米饭\"}]}";
     getMockMvc().perform(get("/foods").accept(MediaType.APPLICATION_JSON))
             .andDo(MockMvcResultHandlers.print())
             .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
@@ -26,7 +26,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void findAll() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":[{\"id\":\"1\",\"name\":\"面包\"},{\"id\":\"2\",\"name\":\"米饭\"}]}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":[{\"id\":\"1\",\"name\":\"面包\"},{\"id\":\"2\",\"name\":\"米饭\"}]}";
     getMockMvc().perform(get("/foods.json"))
         .andDo(MockMvcResultHandlers.print())
         .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
@@ -35,7 +35,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void findOne() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":{\"id\":\"1\",\"name\":\"面包\"}}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":{\"id\":\"1\",\"name\":\"面包\"}}";
     MvcResult result = getMockMvc().perform(get("/foods/1.json"))
         .andDo(MockMvcResultHandlers.print())
         .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
@@ -46,7 +46,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void create() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":{\"id\":\"1\",\"name\":\"面包\"}}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":{\"id\":\"1\",\"name\":\"面包\"}}";
     getMockMvc().perform(post("/foods.json").param("name", "面包"))
         .andDo(MockMvcResultHandlers.print())
         .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
@@ -55,7 +55,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void creates() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":[{\"id\":\"1\",\"name\":\"面包\"},{\"id\":\"2\",\"name\":\"米饭\"}]}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":[{\"id\":\"1\",\"name\":\"面包\"},{\"id\":\"2\",\"name\":\"米饭\"}]}";
     getMockMvc().perform(post("/foods/creates.json").contentType(MediaType.APPLICATION_JSON).content("[{\"name\":\"面包\"},{\"name\":\"米饭\"}]"))
             .andDo(MockMvcResultHandlers.print())
             .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
@@ -64,7 +64,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void update() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":{\"id\":\"1\",\"name\":\"米饭\"}}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":{\"id\":\"1\",\"name\":\"米饭\"}}";
     getMockMvc().perform(put("/foods.json").param("id", "1").param("name", "面包"))
         .andDo(MockMvcResultHandlers.print())
         .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
@@ -73,7 +73,7 @@ public class FoodTest extends AbstractTest{
 
   @Test
   public void _delete() throws Exception{
-    String content = "{\"state\":\"1\",\"data\":[\"1\",\"2\"]}";
+    String content = "{\"state\":\"1\",\"msg\":\"\",\"data\":[\"1\",\"2\"]}";
     getMockMvc().perform(delete("/foods.json").param("ids", "1").param("ids", "2"))
         .andDo(MockMvcResultHandlers.print())
         .andDo(mvcResult -> Assert.assertEquals(content, mvcResult.getResponse().getContentAsString()))
