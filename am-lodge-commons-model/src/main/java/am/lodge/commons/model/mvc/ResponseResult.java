69f9ef94bda7212c350e73b44c335549b0bf5d90
am-wangjin <am-wangjin@gmail.com>
2017-03-12 09:07:59 +0800

    修改状态,增加消息字段

--- am-lodge-commons-model/src/main/java/am/lodge/commons/model/mvc/ResponseResult.java
+++ am-lodge-commons-model/src/main/java/am/lodge/commons/model/mvc/ResponseResult.java
@@ -7,11 +7,13 @@ import java.io.Serializable;
  */
 public class ResponseResult implements Serializable{
 
-  public final static String STATE_ERROR = "-1";
+  public final static String FAILURE = "0";
 
   public final static String SUCCESS = "1";
 
   private String state = SUCCESS;
+
+  private String msg;
 
   private Object data;
 
@@ -29,5 +31,13 @@ public class ResponseResult implements Serializable{
 
   public void setData(Object data){
     this.data = data;
+  }
+
+  public String getMsg(){
+    return msg;
+  }
+
+  public void setMsg(String msg){
+    this.msg = msg;
   }
 }
