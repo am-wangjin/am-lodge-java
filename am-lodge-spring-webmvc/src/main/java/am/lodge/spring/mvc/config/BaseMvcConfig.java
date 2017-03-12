6b829cd5ef0b8400e52018a00b4646c0cf0f6338
am-wangjin <am-wangjin@gmail.com>
2017-03-12 11:17:27 +0800

    使用父类抽象方法初始化

--- am-lodge-spring-webmvc/src/main/java/am/lodge/spring/mvc/config/BaseMvcConfig.java
+++ am-lodge-spring-webmvc/src/main/java/am/lodge/spring/mvc/config/BaseMvcConfig.java
@@ -2,40 +2,31 @@ package am.lodge.spring.mvc.config;
 
 import am.lodge.spring.mvc.servlet.handler.HandlerMethodExceptionResolver;
 import am.lodge.spring.mvc.view.MappingJacksonJsonView;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.List;
-
-import static am.lodge.commons.util.CollectionUtils.list;
 
 /**
  * Created by am on 16-11-13.
  */
 public class BaseMvcConfig extends WebMvcConfigurationSupport{
 
-  @Bean
   @Override
-  public ViewResolver mvcViewResolver() {
-    ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
-    viewResolver.setContentNegotiationManager(this.mvcContentNegotiationManager());
-    viewResolver.setOrder(1);
+  protected void configureViewResolvers(ViewResolverRegistry registry) {
     MappingJacksonJsonView view = new MappingJacksonJsonView();
     view.setServletContext(this.getServletContext());
     view.setApplicationContext(this.getApplicationContext());
-    viewResolver.setDefaultViews(list(view));
+    registry.enableContentNegotiation(view);
 
 
-    InternalResourceViewResolver irvResolver = new InternalResourceViewResolver();
-    irvResolver.setApplicationContext(this.getApplicationContext());
-    irvResolver.setServletContext(this.getServletContext());
-    irvResolver.setOrder(2);
-    viewResolver.setViewResolvers(list(irvResolver));
-    return viewResolver;
+    InternalResourceViewResolver jspResolver = new InternalResourceViewResolver();
+    jspResolver.setApplicationContext(this.getApplicationContext());
+    jspResolver.setServletContext(this.getServletContext());
+    jspResolver.setOrder(1);
+    registry.viewResolver(jspResolver);
   }
 
   @Override
